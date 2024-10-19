25914d8491a1af9bf6de1311e00a6eaa4252dda0
Sujal <118900556+sairevoori8@users.noreply.github.com>
2024-10-19 23:37:00 +0530

    Update description.py

--- description.py
+++ description.py
@@ -4,7 +4,7 @@ from langchain_google_genai import GoogleGenerativeAI
 
 class GenrateText:
     def LlmText(self, givT):
-        llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key="AIzaSyABbWo-PqVU4xmJd2vL5NsczrAUkDWJYk0")
+        llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key="YOUR API")
 
         prompt_template = PromptTemplate.from_template(
             template="You will be given a text which is extracted from an image. Provide details about the product like its company name , manufacture date, and expiry date.there are spelling mistakes and irregularities in the text be careful . Here is the text: {content}. Generate in the following manner: {length}"
